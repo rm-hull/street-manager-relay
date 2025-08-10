1e10e2c8dd65b9a7925efd1196232b3508c15659
Richard Hull <rm_hull@yahoo.co.uk>
2025-08-10 14:19:00 +0100

    Add attribution to search endpoint response (#10)

---
+++ internal/attribution.go
@@ -0,0 +1,5 @@
+package internal
+
+var ATTRIBUTION = []string{
+	"Street Manager Open Data (GOV.UK, Department of Transport), https://department-for-transport-streetmanager.github.io/street-manager-docs/open-data/",
+}
