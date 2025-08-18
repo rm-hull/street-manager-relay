b2895eafce893a2e999e5d2d7f1234466aba50bd
Richard Hull <rm_hull@yahoo.co.uk>
2025-08-18 20:23:45 +0100

    Consistent union columns

--- internal/sql/ref_data.sql
+++ internal/sql/ref_data.sql
@@ -2,26 +2,26 @@ SELECT 'permit_status' AS facet, permit_status AS value, COUNT(*) AS cnt
 FROM events
 GROUP BY permit_status
 UNION ALL
-SELECT 'traffic_management_type_ref', traffic_management_type_ref, COUNT(*)
+SELECT 'traffic_management_type_ref' AS facet, traffic_management_type_ref AS value, COUNT(*) AS cnt
 FROM events
 GROUP BY traffic_management_type_ref
 UNION ALL
-SELECT 'work_status_ref', work_status_ref, COUNT(*)
+SELECT 'work_status_ref' AS facet, work_status_ref AS value, COUNT(*) AS cnt
 FROM events
 GROUP BY work_status_ref
 UNION ALL
-SELECT 'work_category_ref', work_category_ref, COUNT(*)
+SELECT 'work_category_ref' AS facet, work_category_ref AS value, COUNT(*) AS cnt
 FROM events
 GROUP BY work_category_ref
 UNION ALL
-SELECT 'road_category', road_category, COUNT(*)
+SELECT 'road_category' AS facet, road_category AS value, COUNT(*) AS cnt
 FROM events
 GROUP BY road_category
 UNION ALL
-SELECT 'highway_authority', highway_authority, COUNT(*)
+SELECT 'highway_authority' AS facet, highway_authority AS value, COUNT(*) AS cnt
 FROM events
 GROUP BY highway_authority
 UNION ALL
-SELECT 'promoter_organisation', promoter_organisation, COUNT(*)
+SELECT 'promoter_organisation' AS facet, promoter_organisation AS value, COUNT(*) AS cnt
 FROM events
 GROUP BY promoter_organisation;
