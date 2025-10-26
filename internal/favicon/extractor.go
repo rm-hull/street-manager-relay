5991aa4679e639bb476b70fb115ecd5b5906518f
Richard Hull <rm_hull@yahoo.co.uk>
2025-10-26 01:22:04 +0100

    Prioritize icons rather than og:

--- internal/favicon/extractor.go
+++ internal/favicon/extractor.go
@@ -15,9 +15,10 @@ import (
 )
 
 type IconInfo struct {
-	Href string
-	Src  string
-	Size int
+	Href     string
+	Src      string
+	Size     int
+	Priority bool
 }
 
 func resolveURL(base, href string) string {
@@ -108,7 +109,12 @@ func Extract(url string) (*IconInfo, error) {
 		doc.Find(selector).Each(func(i int, s *goquery.Selection) {
 			if href, ok := s.Attr(attr); ok {
 				size := parseSize(s.AttrOr("sizes", "0x0"))
-				icons = append(icons, IconInfo{Href: resolveURL(url, href), Size: size, Src: selector})
+				icons = append(icons, IconInfo{
+					Href:     resolveURL(url, href),
+					Size:     size,
+					Src:      selector,
+					Priority: attr == "href",
+				})
 			}
 		})
 	}
@@ -117,8 +123,12 @@ func Extract(url string) (*IconInfo, error) {
 		return nil, fmt.Errorf("no icons found for: %s", url)
 	}
 
-	sort.Slice(icons, func(i, j int) bool {
-		return icons[i].Size > icons[j].Size
+	sort.SliceStable(icons, func(i, j int) bool {
+		if icons[i].Priority == icons[j].Priority {
+			return icons[i].Size > icons[j].Size
+		}
+
+		return icons[i].Priority
 	})
 
 	return &icons[0], nil
